e490aeea761a4751223fcf04c5a3b4e722982e9f
Sukhmeet3098 <sukhmeetsingh3098@gmail.com>
2026-02-19 23:55:37 -0500

    Javascript ArithmeticOperators  Complete

---
+++ script.js
@@ -0,0 +1,16 @@
+// This is Single Line Comment
+console.log("Hello World!!");
+
+/* This is a 
+multi line Comment */
+
+//Artimetic Opertor
+let a = 5;
+let b = 2;
+
+console.log("a + b: ", a + b);
+console.log("a - b: ", a - b);
+console.log("a * b: ", a * b);
+console.log("a / b: ", a / b);
+console.log("a % b: ", a % b);
+console.log("a ** b: ", a ** b);
