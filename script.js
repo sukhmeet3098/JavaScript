19382ea5a0e77556b930076ad7ba98c4ddfcd2fb
Sukhmeet3098 <sukhmeetsingh3098@gmail.com>
2026-02-20 00:07:06 -0500

    Javascript Uniary Operators Complete

--- script.js
+++ script.js
@@ -8,9 +8,29 @@ multi line Comment */
 let a = 5;
 let b = 2;
 
-console.log("a + b: ", a + b);
-console.log("a - b: ", a - b);
-console.log("a * b: ", a * b);
-console.log("a / b: ", a / b);
-console.log("a % b: ", a % b);
-console.log("a ** b: ", a ** b);
+// console.log("a + b: ", a + b);
+// console.log("a - b: ", a - b);
+// console.log("a * b: ", a * b);
+// console.log("a / b: ", a / b);
+// console.log("a % b: ", a % b);
+// console.log("a ** b: ", a ** b);
+
+//Uniary Opeartors
+a++;
+console.log("a = ", a);
+a--;
+console.log("a = ", a);
+
+// Pre(++a) and Post(a++) Increment
+// console.log("a = ", ++a);
+// console.log("a = ", a);
+
+// console.log("a = ", a++);
+// console.log("a = ", a);
+
+// Pre(++a) and Post(a++) Decrement
+console.log("a = ", --a); // 4
+console.log("a = ", a); //4
+
+console.log("a = ", a--); //5
+console.log("a = ", a); //4
