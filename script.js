73674aaabf368ec79b21383d30e639f8ed3664b4
Sukhmeet3098 <sukhmeetsingh3098@gmail.com>
2026-03-01 18:51:05 -0500

    Opeartors and Conditional Statements in JS Completed

--- script.js
+++ script.js
@@ -28,7 +28,7 @@ multi line Comment */
 // console.log("a = ", a++);
 // console.log("a = ", a);
 
-// Pre(++a) and Post(a++) Decrement
+// Pre(--a) and Post(a--) Decrement
 // console.log("a = ", --a); // 4
 // console.log("a = ", a); //4
 
@@ -49,14 +49,89 @@ multi line Comment */
 
 //Comparison Operators
 
-let a = 5;
-let b = 2;
+// let a = 5;
+// let b = 2;
 
-console.log("a == b: ", a == b); // false
-console.log("a === b: ", a === b); // false
-console.log("a != b: ", a != b); // true
-console.log("a !== b: ", a !== b); // true
-console.log("a > b: ", a > b); // true
-console.log("a >= b: ", a >= b); // true
-console.log("a <= b: ", a <= b); // false
-console.log("a < b: ", a < b); // false
+// console.log("a == b: ", a == b); // false
+// console.log("a === b: ", a === b); // false
+// console.log("a != b: ", a != b); // true
+// console.log("a !== b: ", a !== b); // true
+// console.log("a > b: ", a > b); // true
+// console.log("a >= b: ", a >= b); // true
+// console.log("a <= b: ", a <= b); // false
+// console.log("a < b: ", a < b); // false
+
+// Logical Operators
+
+// let a = 6;
+// let b = 5;
+
+// // AND Operator &&
+// console.log("(a > b) && (a === 6) = ", a > b && a === 6); // true
+// console.log("(a < b) && (a === 6) = ", a < b && a === 6); // false
+
+// // OR Operator ||
+
+// console.log("(a > b) || (a === 6) = ", a < b || a === 6); // true
+// console.log("(a < b) || (a === 6) = ", a < b || a === 5); // false
+
+// // NOT Operator !
+// console.log("!(a === 6) = ", !(a === 6)); // false
+// console.log("!(a === 5) = ", !(a === 5)); // true
+
+// Conditional Statements
+
+// let age = 25;
+// //IF statement
+// if (age <= 18) {
+//   console.log("Adult");
+// }
+
+// if (age < 18) {
+//   console.log("Not Adult");
+// }
+
+// //IF - ELSE Statement
+// if (age >= 18) {
+//   console.log("Adult");
+// } else {
+//   console.log("Not Adult");
+// }
+// // ELSE IF Statement
+// if (age < 18) {
+//   console.log("Teenager");
+// } else if (age < 60) {
+//   console.log("Senior");
+// } else {
+//   console.log("Middle");
+// }
+// // Ternary Operators
+
+// // condition ? true output : false output;
+// age > 18 ? console.log("Adult") : console.log("Not Adult");
+
+// Problem Statement 1 (Check number is multiple of 5.)
+
+// let num = prompt("Enter a Number");
+
+// if (num % 5 === 0) {
+//   console.log(num, "is a multiple of 5.");
+// } else {
+//   console.log(num, "is NOT a multiple of 5.");
+// }
+
+// // Problem 2 (Give Grades to Students Accordingly to their scores.)
+
+// let score = prompt("Enter the Marks");
+
+// if (score >= 90 && score <= 100) {
+//   console.log("Grade: A");
+// } else if (score >= 70 && score <= 89) {
+//   console.log("Grade: B");
+// } else if (score >= 60 && score <= 69) {
+//   console.log("Grade: C");
+// } else if (score >= 50 && score <= 59) {
+//   console.log("Grade: D");
+// } else {
+//   console.log("Grade: F");
+// }
